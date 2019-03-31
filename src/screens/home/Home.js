87d57f4502f5f85172e60d8ebdec2105a35d1869
Alberto Blanco <alberto@basenet.nl>
2019-03-31 22:43:32 +0200

    Lint all files

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -5,7 +5,6 @@ import TwitterLogo from '../../assets/logo-twitter.svg';
 import YoutubeLogo from '../../assets/logo-youtube.png';
 import GithubLogo from '../../assets/logo-github.svg';
 
-
 const LogoLink = ({ link, src }) => (
   <a href={link}>
     <img src={src} alt="" className="Home-small-img" />
@@ -17,7 +16,10 @@ const Home = () => (
     <img src={FGLogo} alt="" className="Home-img" />
     <div className="Home-logos-container">
       <LogoLink src={TwitterLogo} link="https://twitter.com/Flipper_Gang" />
-      <LogoLink src={YoutubeLogo} link="https://www.youtube.com/channel/UCdYGpwhbx2DtpR5jMPLEIeA" />
+      <LogoLink
+        src={YoutubeLogo}
+        link="https://www.youtube.com/channel/UCdYGpwhbx2DtpR5jMPLEIeA"
+      />
       <LogoLink src={GithubLogo} link="https://github.com/ItsNoHax" />
     </div>
   </div>
