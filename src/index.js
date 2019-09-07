5ceca05e9c39670ee3dc88e128c32146b4ea5fdb
Alberto Blanco <alberto@basenet.nl>
2019-09-08 00:14:46 +0200

    Update project to create-react-app version 3.1.1

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
 import Home from './screens/home/Home';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
+serviceWorker.register();
